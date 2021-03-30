c41a6d208210048e37afa8fdd40a33c8a09eedaf
Carter <it@gibbshn.me>
2021-03-30 17:14:44 -0600

    Commits before we add the responses randomizer

---
+++ responses.js
@@ -0,0 +1,18 @@
+let oppressResponses = [
+    'I\'ve decided to oppress you.',
+    ''
+  
+  
+  ]
+  let praiseResponses = [
+    'I\'ve decided to praise you.',
+    ''
+  
+  
+  ]
+  let banResponses = [
+    '@user... We don\'t say things like:\n@msg\n on this server!',
+    ''
+  
+  
+  ]
